fc72ad937c6e8ea11cefce66a00a87931f577013
Pratham Patil <pratham0895@gmail.com>
2025-12-26 04:50:19 -0500

    Uncomment CREATE DATABASE statement in wire.sql

--- wire.sql
+++ wire.sql
@@ -1,4 +1,4 @@
--- CREATE DATABASE Wire_Tek;
+CREATE DATABASE Wire_Tek;
 USE Wire_Tek;
 create table Cables (Reel_no varchar(50), cable_print varchar(250), top int,
  bot int, total_length int, remarks varchar(250));
