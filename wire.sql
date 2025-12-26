beb43fcf9546b27bd1a0c2d31065ac909eb83649
pratham08 <pratham0895@gmail.com>
2025-12-26 04:42:17 -0500

    new sql file

---
+++ wire.sql
@@ -0,0 +1,5 @@
+-- CREATE DATABASE Wire_Tek;
+USE Wire_Tek;
+create table Cables (Reel_no varchar(50), cable_print varchar(250), top int,
+ bot int, total_length int, remarks varchar(250));
+select * from cables;
